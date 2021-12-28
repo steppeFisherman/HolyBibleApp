0d20cee319a424bfdc0db3ff1fb73871c8cbd486
redfox <stepperobot@gmail.com>
2021-12-28 23:24:42 +0300

    HB01 presentation layer for books

---
+++ app/src/main/java/com/example/holybibleapp/presentation/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.holybibleapp.presentation
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.holybibleapp.core.Abstract
+import com.example.holybibleapp.core.Book
+import com.example.holybibleapp.domain.BooksDomainToUiMapper
+import com.example.holybibleapp.domain.BooksInteractor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    private val booksInteractor: BooksInteractor,
+    private val mapper: BooksDomainToUiMapper,
+    private val communication: BooksCommunication
+): ViewModel() {
+
+    fun fetchBooks() = viewModelScope.launch(Dispatchers.IO){
+        val result: BooksUi = booksInteractor.fetchBooks().map(mapper)
+        with(Dispatchers.Main){
+           result.map(Abstract.Mapper.Empty())
+        }
+    }
+
+    fun observe(owner: LifecycleOwner, observer: Observer<List<Book>>){
+        communication.observeSuccess(owner, observer)
+    }
+}
