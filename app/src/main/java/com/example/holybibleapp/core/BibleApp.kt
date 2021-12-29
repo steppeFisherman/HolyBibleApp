0a8ab285b53e351c43962ab223bfcd0c7e5aa600
redfox <stepperobot@gmail.com>
2021-12-29 09:46:48 +0300

    HB01 presentation layer for books

--- app/src/main/java/com/example/holybibleapp/core/BibleApp.kt
+++ app/src/main/java/com/example/holybibleapp/core/BibleApp.kt
@@ -1,54 +1,24 @@
 package com.example.holybibleapp.core
 
 import android.app.Application
-import com.example.holybibleapp.data.BooksCloudDataSource
-import com.example.holybibleapp.data.BooksCloudMapper
-import com.example.holybibleapp.data.BooksRepository
-import com.example.holybibleapp.data.cache.BookCacheMapper
-import com.example.holybibleapp.data.cache.BooksCacheDataSource
-import com.example.holybibleapp.data.cache.BooksCacheMapper
-import com.example.holybibleapp.data.cache.RealmProvider
-import com.example.holybibleapp.data.net.BookCloudMapper
-import com.example.holybibleapp.data.net.BooksService
-import com.example.holybibleapp.domain.BaseBooksDataToDomainMapper
 import com.example.holybibleapp.domain.BooksInteractor
-import retrofit2.Retrofit
+import com.example.holybibleapp.presentation.BaseBooksDomainToUiMapper
+import com.example.holybibleapp.presentation.BooksCommunication
+import com.example.holybibleapp.presentation.MainViewModel
+import com.example.holybibleapp.presentation.ResourceProvider
 
 class BibleApp: Application() {
 
-    private companion object {
-        const val BASE_URL = "https://bible-go-api.rkeplin.com/v1/"
-    }
+    lateinit var mainViewModel: MainViewModel
 
-  //  lateinit var mainViewModel: MainViewModel
     override fun onCreate() {
         super.onCreate()
-//
-//        mainViewModel = MainViewModel(
-//            booksInteractor,
-//            BaseBooksDomainToUiMapper(BooksCommunication.Base(), ResourceProvider.Base(this))
-//        )
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-                //todo log http calls
-            .build()
-        val service = retrofit.create(BooksService::class.java)
 
-         val cloudDataSource = BooksCloudDataSource.Base(service)
-         val cacheDataSource = BooksCacheDataSource.Base(RealmProvider.Base())
-         val booksCloudMapper = BooksCloudMapper.Base(BookCloudMapper.Base())
-         val booksCacheMapper =  BooksCacheMapper.Base(BookCacheMapper.Base())
+        val booksInteractor: BooksInteractor = TODO()
 
-        val booksRepository = BooksRepository.Base(
-            cloudDataSource,
-            cacheDataSource,
-            booksCloudMapper,
-            booksCacheMapper
-        )
-
-        val booksInteractor = BooksInteractor.Base(
-            booksRepository,
-            BaseBooksDataToDomainMapper()
+        mainViewModel = MainViewModel(
+            booksInteractor,
+            BaseBooksDomainToUiMapper(BooksCommunication.Base(), ResourceProvider.Base(this))
         )
     }
 }
--- app/src/main/java/com/example/holybibleapp/presentation/BooksUi.kt
+++ app/src/main/java/com/example/holybibleapp/presentation/BooksUi.kt
@@ -1,7 +1,32 @@
 package com.example.holybibleapp.presentation
 
+import com.example.holybibleapp.R
 import com.example.holybibleapp.core.Abstract
+import com.example.holybibleapp.core.Book
+import com.example.holybibleapp.domain.ErrorType
 
-sealed class BooksUi: Abstract.Object<Unit,Abstract.Mapper.Empty>(){
+sealed class BooksUi : Abstract.Object<Unit, Abstract.Mapper.Empty>() {
 
+    class Success(
+        private val communication: BooksCommunication,
+        private val books: List<Book>
+    ) : BooksUi() {
+        override fun map(mapper: Abstract.Mapper.Empty) = communication.show(books)
+    }
+
+    class Fail(
+        private val communication: BooksCommunication,
+        private val errorType: ErrorType,
+        private val resourceProvider: ResourceProvider //ErrorTypeHandler
+    ) : BooksUi() {
+        override fun map(mapper: Abstract.Mapper.Empty) {
+            val messageId = when (errorType) { //todo move to other class
+                ErrorType.NO_CONNECTION -> R.string.no_connection_message
+                ErrorType.SERVICE_UNAVAILABLE -> R.string.service_unavailable_message
+                else -> R.string.something_went_wrong
+            }
+            communication.show(resourceProvider.getString(messageId))
+        }
+
+    }
 }
